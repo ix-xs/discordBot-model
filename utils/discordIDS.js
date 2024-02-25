697b0f9a954b7d5296c25022af2ee6ec5cfba28c
ix-xs <114710533+ix-xs@users.noreply.github.com>
2024-02-25 16:36:24 +0100

    Create discordIDS.js

---
+++ utils/discordIDS.js
@@ -0,0 +1,27 @@
+module.exports =
+
+{
+
+	botId:"", // Id Discord du bot
+	devId:"", // Id Discord du développeur
+	guildId:"", // Id Discord du serveur
+
+	// Ids Discord des salons
+	channels:{
+		errors:"",
+		"@categories":{ // Ids des catégories de salon
+
+		},
+	},
+
+	// Ids Discord des roles
+	roles:{
+
+	},
+
+	// Ids Discord des emojis
+	emojis:{
+
+	},
+
+};
